28df5c4920ad57aeee6c8673d964e394529eb34c
johnyeryck <johnybombado@gmail.com>
2025-09-14 20:30:50 -0300

    feat: implement account creation with email confirmation and add .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import pool from './db.js';
+import { createaccount } from './createaccount.js';
 
 const app = express()
 app.use(express.json())
-
 
 
 // Rota Principal
@@ -16,17 +16,9 @@ app.get("/" ,(req, res)=>{
 app.get("/criarconta" ,(req, res)=>{
     res.status(200).send("criar conta")
 })
-app.post("/criarconta", async(req , res)=>{
-    const {email,password,username} = req.body
-    let id = BigInt(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER));
 
-    try{
-        await pool.query("INSERT INTO usuarios (email,password,username,id) VALUES($1,$2,$3,$4) RETURNING*" , [email,password,username,id])
-    }catch(err){
-        console.log(err)
-    }
+app.post("/criarconta" , createaccount)
 
-})
 
 // Rota de Login
 app.post("/login" , async (req, res)=>{
