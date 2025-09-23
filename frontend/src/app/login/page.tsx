7557b9d1200eca9ae8c557e8111d74a7373ff7ad
johnyeryck <johnybombado@gmail.com>
2025-09-23 13:36:38 -0300

    feat: initialize Next.js project with essential files and configurations

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+import {useForm} from 'react-hook-form'
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {register , handleSubmit , formState : {errors}} = useForm()
+ const onsubmit = async()=>{
+     const res = await fetch("http://localhost:4000/login", {
+        method : "POST"
+      })
+      console.log(res.status)
+ }
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-black" >
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold text-center text-black">Login</h1>
+        <form className="space-y-4" onSubmit={handleSubmit(onsubmit)}>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              {...register("email" , {required : "Obrigatorio"})}
+            />
+         
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              Senha
+            </label>
+            <input
+              type="password"
+              id="password"
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              {...register("password" , {required : "Obrigatorio"})}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
