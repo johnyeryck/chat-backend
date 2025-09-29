8cd57fa2d1a43014b79739af30ea60c60fdc98d9
johnyeryck <johnybombado@gmail.com>
2025-09-29 13:05:22 -0300

    component  animate

---
+++ frontend/src/app/components/loadspinner.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function LoadingSpinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className="
+          h-8 w-8
+          rounded-full
+          border-4 border-solid border-gray-200 border-t-blue-500
+          animate-spin
+        "
+      ></div>
+    </div>
+  );
+}
