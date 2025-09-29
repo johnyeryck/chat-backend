42de78dc3dc9f6f1ee563da7d64311a9a8dc5449
johnyeryck <johnybombado@gmail.com>
2025-09-29 13:01:26 -0300

    Send email confirmation of account creation

--- backend/emailconfig.js
+++ backend/emailconfig.js
@@ -1,7 +1,7 @@
-import nodemailer from "nodemailer"
-import {dirname, join} from 'path'
-import {fileURLToPath} from 'url'
-import dotenv from 'dotenv'
+import nodemailer from "nodemailer";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 const file = fileURLToPath(import.meta.url);
 const __dirname = dirname(file);
 dotenv.config({ path: join(__dirname, "../.env") });
