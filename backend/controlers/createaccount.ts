9b223019b091ccccf81ff5994d5704f52f7ea9e8
johnyeryck <johnybombado@gmail.com>
2025-10-08 10:02:05 -0300

    feat: implement account creation middleware with email verification

---
+++ backend/controlers/createaccount.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { transport } from "../emailconfig.js";
+import pool from "../db.js";
+import bcrypt from "bcryptjs";
+import { emaildata } from "../emailTransporter.js";
+import { Getusername } from "../createUsername.js";
+
+// MIDDLEWARE OF ACCOUNT CREATION
+export const createaccount = async (req : any, res : any) => {
+  const { email, password } = req.body;
+
+ const user = Getusername(email)
+ console.log(user)
+  const hash = await bcrypt.hash(password, 10);
+  try {
+    const consultation = await pool.query(
+      "SELECT email FROM usuarios WHERE email = $1",
+      [email],
+    );
+    if (consultation.rowCount === 0) {
+      const token = jwt.sign({ email, hash,user }, "segredo", {
+        expiresIn: "5min",
+      });
+      const url = `http://localhost:3000/confirmEmail/${token}`;
+
+      transport.sendMail(emaildata(email,url), (error : any, info : any) => {
+        if (error) return res.status(500).send(error);
+        return res.sendStatus(200);
+      });
+    } else {
+      return res.status(400).send("Email já cadastrado");
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+};
