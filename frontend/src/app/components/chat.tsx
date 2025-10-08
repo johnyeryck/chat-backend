8dca7d755e9aa36d774433cdad2ae3715b342188
johnyeryck <johnybombado@gmail.com>
2025-10-08 10:00:57 -0300

    format Chat component for improved readability

--- frontend/src/app/components/chat.tsx
+++ frontend/src/app/components/chat.tsx
@@ -2,8 +2,12 @@ export default function Chat() {
   return (
     <div className="">
       <div className="border w-1/3 h-140 mt-20 text-white ml-auto mr-auto ">
-      <input type="text" className=" border border-white pl-2" placeholder="Envie uma mensagem"/>
-      <button className="border border-[#0cf187]">Enviar</button>
+        <input
+          type="text"
+          className=" border border-white pl-2"
+          placeholder="Envie uma mensagem"
+        />
+        <button className="border border-[#0cf187]">Enviar</button>
       </div>
     </div>
   );
