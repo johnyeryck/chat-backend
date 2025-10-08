b8c297f3b84a2d75fe8e5484599c35823105b5d8
johnyeryck <johnybombado@gmail.com>
2025-10-08 10:00:46 -0300

    refactor Home component structure and clean up unused imports

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,33 +1,24 @@
-'use client'  
-import Image from "next/image";
-import user from '../../public/do-utilizador.png'
+"use client";
+import Navbar from "./components/navbar";
 import { useEffect } from "react";
 export default function Home() {
-
- useEffect(()=>{
-    const consultatio = async ()=>{
+  useEffect(() => {
+    const consultatio = async () => {
       const res = await fetch("http://localhost:4000", {
-        method : "GET",
-        credentials : "include"
-      })
-      if(!res.ok){
-        throw new Error("Falha na requisição")
+        method: "GET",
+        credentials: "include",
+      });
+      if (!res.ok) {
+        throw new Error("Falha na requisição");
       }
-    }
-    consultatio()
-  })
-
-
+    };
+    consultatio();
+  });
 
   return (
-    <div className="">
-        <article className="border  ml-auto h-20">
-          <div className="rounded-full size-10  pt-3 ml-auto mr-2">
-            <Image  src={user} alt="Foto de perfil"/>
-          </div>
-        </article>
-
-        <div className="border w-[90%] ml-auto mr-auto mt-20 h-120"></div>
-    </div>
+    <>
+      <Navbar />
+      <div className="border w-[90%] ml-auto mr-auto mt-20 h-120"></div>
+      </>
   );
 }
