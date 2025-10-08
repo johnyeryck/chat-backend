8e38d82c6fd69150ae1a1db207ae6781ab9994f1
johnyeryck <johnybombado@gmail.com>
2025-10-08 10:01:05 -0300

    refactor LoginPage component for improved code readability and consistency

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,37 +1,44 @@
-'use client';
-import {useForm} from 'react-hook-form'
-import { useState } from 'react';
-import LoadingSpinner from '../components/loadspinner';
+"use client";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import LoadingSpinner from "../components/loadspinner";
 
 export default function LoginPage() {
   const [loading, setloading] = useState<boolean>(false);
-  const {register , handleSubmit , formState : {errors}} = useForm()
-  
-  const onsubmit = async(e : any)=>{
-   const data = {email: e.email , password : e.password}
-    try{
-        const res = await fetch("http://localhost:4000/login", {
-        method : "POST",
-        body : JSON.stringify(data),
-        headers : {
-          "Content-Type" : "application/json"
-        }
-      })
-      if(res.status === 200){
-        setloading(false)
-        window.location.href = "http://localhost:3000" 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onsubmit = async (e: any) => {
+    const data = { email: e.email, password: e.password };
+    try {
+      const res = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.status === 200) {
+        setloading(false);
+        window.location.href = "http://localhost:3000";
       }
-    }catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
- }
+  };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-black" >
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-black">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
         <h1 className="text-2xl font-bold text-center text-black">Login</h1>
         <form className="space-y-4" onSubmit={handleSubmit(onsubmit)}>
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
               Email
             </label>
             <input
@@ -39,12 +46,14 @@ export default function LoginPage() {
               id="email"
               required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-              {...register("email" , {required : "Obrigatorio"})}
+              {...register("email", { required: "Obrigatorio" })}
             />
-         
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
               Senha
             </label>
             <input
@@ -52,14 +61,14 @@ export default function LoginPage() {
               id="password"
               required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-              {...register("password" , {required : "Obrigatorio"  })}
+              {...register("password", { required: "Obrigatorio" })}
             />
           </div>
           <button
             type="submit"
             className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
-            {loading ? <LoadingSpinner/> : "Entrar" }
+            {loading ? <LoadingSpinner /> : "Entrar"}
           </button>
         </form>
       </div>
