5e06ecc3c2681e0c317b976b08565166b9d276cb
johnyeryck <johnybombado@gmail.com>
2025-10-11 22:26:47 -0300

    fix: improve formatting and consistency in login.ts

--- backend/controlers/login.ts
+++ backend/controlers/login.ts
@@ -1,27 +1,21 @@
 import pool from "../db.js";
 import bcrypt from "bcryptjs";
-import jwt from 'jsonwebtoken'
-export const login = async (req, res) => {
+import type { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const consultation = await pool.query(
     "SELECT * FROM usuarios WHERE email = $1",
     [email],
   );
 
-  if(consultation.rowCount === 0){
-      res.status(401).send("Email Inválido")
+  if (consultation.rowCount === 0) {
+    res.status(401).send("Email Inválido");
   }
-  const hashed = consultation.rows[0].password
-  const comp = bcrypt.compare(password,hashed)
+  const hashed = consultation.rows[0].password;
+  const comp = await bcrypt.compare(password, hashed);
+  console.log(comp);
   if (comp) {
-    // const token = jwt.sign("secret")
-    res.cookie("secretToken", "daksmdkas", {
-      httpOnly: true,
-      sameSite: "strict",
-      secure: false,
-      path: "/",
-      maxAge: 60 * 60 * 24 * 30,
-    });
     res.sendStatus(200);
   }
   res.sendStatus(400);
