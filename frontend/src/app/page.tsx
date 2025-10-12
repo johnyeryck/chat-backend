529cbc8f8d312a2738ea9be22be908681bdc1987
johnyeryck <johnybombado@gmail.com>
2025-10-11 22:27:20 -0300

    feat: restructure Home component layout and enhance message input section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Navbar from "./components/navbar";
+import Image from "next/image";
 import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
@@ -16,9 +17,24 @@ export default function Home() {
   });
 
   return (
-    <>
+    <header className="bg-[#0e0f19]">
       <Navbar />
-      <div className="border w-[90%] ml-auto mr-auto mt-20 h-120"></div>
-      </>
+      <main className="flex justify-evenly w-[80%] ml-auto mr-auto">
+        <div className="shadow shadow-gray-800 w-1/3  mt-20 h-120 rounded-lg  bg-[#0D0E1C]"></div>
+
+        <div className=" shadow shadow-gray-800 w-1/3  mt-20 h-120 rounded-lg bg-[#0D0E1C]">
+          <div className=" flex justify-evenly ">
+            <input
+              type="text"
+              className="border border-[#1FFFE1]  rounded-2xl pl-3"
+              placeholder="Envie Uma Mensagem"
+            />
+            <button type="submit" className="cursor-pointer">
+              <Image src="/mandar.png" alt="enviar" width={20} height={20} />
+            </button>
+          </div>
+        </div>
+      </main>
+    </header>
   );
 }
