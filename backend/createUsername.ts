3a003a81c1017794e3b0bf90d121536323651165
johnyeryck <johnybombado@gmail.com>
2025-10-11 22:25:48 -0300

    fix: improve formatting and readability of Getusername function

--- backend/createUsername.ts
+++ backend/createUsername.ts
@@ -1,29 +1,27 @@
-export function Getusername(user : string){
-      //  REMOVED @ AND PUT IN LOWERCASE
-     const emailRemoved = user.replace("@gmail.com" ,"")
-     const username : any = emailRemoved.toLowerCase()
+export function Getusername(user: string) {
+  //  REMOVED @ AND PUT IN LOWERCASE
+  const emailRemoved = user.replace("@gmail.com", "");
+  const username: any = emailRemoved.toLowerCase();
 
-     // TRANSFORM IN A LIST 
-    const leters = "abcdefghijklmnopqrstuvwxyz"
-    const alfabeto = leters.split("")
+  // TRANSFORM IN A LIST
+  const leters = "abcdefghijklmnopqrstuvwxyz";
+  const alfabeto = leters.split("");
 
-    let i = 0
-    let newlist = ""
+  let i = 0;
+  let newlist = "";
 
-   alfabeto.forEach((leter , index)=>{
-      for(const i in username){
-        if(leter === username[i]){
-          newlist += index
-        }
+  alfabeto.forEach((leter, index) => {
+    for (const i in username) {
+      if (leter === username[i]) {
+        newlist += index;
       }
-    })
-    if(username.length > 5){
-      const end = username.length / 2
-      const numersOfuser = newlist.slice(0,end)
-      return username + numersOfuser
     }
-    else{
-      return username + newlist
-    }
-
+  });
+  if (username.length > 5) {
+    const end = username.length / 2;
+    const numersOfuser = newlist.slice(0, end);
+    return username + numersOfuser;
+  } else {
+    return username + newlist;
   }
+}
