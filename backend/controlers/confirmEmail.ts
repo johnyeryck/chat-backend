ec2d0e07d60c3136b0741b48b51b0c16fe59037b
johnyeryck <johnybombado@gmail.com>
2025-10-13 20:54:24 -0300

    removed try and catch and set new class error

--- backend/controlers/confirmEmail.ts
+++ backend/controlers/confirmEmail.ts
@@ -7,11 +7,14 @@ export const ConfirmEmail = async (req: Request, res: Response) => {
   let id = BigInt(Math.random() * Number.MAX_SAFE_INTEGER);
   const { token } = req.query;
   const decode = jwt.decode(token, "segredo");
-  const { hash, email, user } = decode;
-  console.log();
 
+  if(!decode) throw new errosHandle("Token não autorizado" , 401)
+  const { hash, email, user } = decode;
   const response = await pool.query(
     "INSERT INTO usuarios (email , password , id,username) VALUES($1,$2,$3,$4)",
     [email, hash, id, user]
   );
+    return res.sendStatus(201)
+
+  console.log(response)
 };
