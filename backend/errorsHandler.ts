fb0750f1e6375b9dbb709eb22696fbc8a89e78d5
johnyeryck <johnybombado@gmail.com>
2025-10-20 22:05:57 -0300

    feat

--- backend/errorsHandler.ts
+++ backend/errorsHandler.ts
@@ -1,21 +1,31 @@
-  export default class ErrorHandler extends Error {
-    public readonly statuscode: number;
-    constructor(menssage: string, statuscode: number) {
-      super(menssage);
-      this.statuscode = statuscode;
-    }
+export default class ErrorHandler extends Error {
+  public readonly statuscode: number;
+  constructor(menssage: string, statuscode: number) {
+    super(menssage);
+    this.statuscode = statuscode;
   }
+}
 
-  export class notFoundError extends ErrorHandler {
-    constructor(message : string){
-      super(message , 404)
-    }
+export class notFoundError extends ErrorHandler {
+  constructor(message: string) {
+    super(message, 404);
   }
+}
 
-  export class NotAuthorized extends ErrorHandler {
-    constructor(message : string){
-      super(message , 401)
-    }
+export class NotAuthorized extends ErrorHandler {
+  constructor(message: string) {
+    super(message, 401);
   }
+}
 
-  
+export class BadRequest extends ErrorHandler {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class ManyRequests extends ErrorHandler {
+  constructor(message: string) {
+    super(message, 429);
+  }
+}
