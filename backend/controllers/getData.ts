31714f5c7c0d234bd87b4ba16af1d6698e403264
johnyeryck <johnybombado@gmail.com>
2025-10-20 22:23:26 -0300

    Fetch de dados do usuario

--- backend/controllers/getData.ts
+++ backend/controllers/getData.ts
@@ -3,15 +3,15 @@ import pool from "../db.js";
 import type { Request, Response } from "express";
 
 export const Getdata = async (req: Request, res: Response) => {
-  const { token } = req.params;
+  const { token } = req.query;
+  console.log(token);
   const decode = jwt.decode(token, "secret");
-  const { email } = decode;
-  const consult = await pool.query(
-    "SELECT username FROM usuarios WHERE email = $1",
-    [email]
-  );
-  const username = consult.rows[0].username;
-  res.json({
-    user: username,
+  const { username } = decode;
+  const consult = await pool.query("SELECT username FROM usuarios");
+  console.log(consult.rows);
+  const users = consult.rows[0].username;
+  res.status(200).json({
+    users: users,
+    userLoged: username,
   });
 };
